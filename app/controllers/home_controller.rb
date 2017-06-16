0f258b243e21c51a2d33939ecc541f954f1babf3
ilentt <ilentt@ilenlab.com>
2017-06-16 19:32:07 +0700

    Update menu content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,12 @@ class HomeController < ApplicationController
   
   def menu
   	@sections = Section.all
-  	if params[:section_id].present?
-  		@current_section = Section.find(params[:section_id])
-  	else 
-  		@current_section = Section.all.first
-  	end
-
+    if params[:section_id].present?
+    	@current_section = Section.find(params[:section_id])
+    	@food_items = FoodItem.where(section_id: @current_section)
+    else 
+    	@food_items = FoodItem.all
+    end
   end
 
   def contact_us
