043ce1cac22f3422a71c3a1e0fa259fe1b700a3b
ilentt <ilentt@ilenlab.com>
2017-06-21 03:16:40 +0700

    Refactoring code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
 	def active_class(link_path)
 		current_page?(link_path) ? "btn-primary" : ""
 	end
+	def menu_active(link_path)
+		current_page?(link_path) ? "active" : ""
+	end
 end
